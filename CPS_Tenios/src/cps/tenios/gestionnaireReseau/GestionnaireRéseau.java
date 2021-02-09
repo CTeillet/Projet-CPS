f7a6844eaee13cdfd6e1cbdfe4a171feb5b0cc25
alexc <alexc@LAPTOP-BA1QURHD>
2021-02-09 14:58:05 +0100

    début composant

---
+++ CPS_Tenios/src/cps/tenios/gestionnaireReseau/GestionnaireRéseau.java
@@ -0,0 +1,12 @@
+package cps.tenios.gestionnaireReseau;
+
+import fr.sorbonne_u.components.AbstractComponent;
+
+public class GestionnaireRéseau extends AbstractComponent {
+
+	protected GestionnaireRéseau(int nbThreads, int nbSchedulableThreads) {
+		super(nbThreads, nbSchedulableThreads);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ CPS_Tenios/src/cps/tenios/node/Node.java
@@ -0,0 +1,12 @@
+package cps.tenios.node;
+
+import fr.sorbonne_u.components.AbstractComponent;
+
+public class Node extends AbstractComponent{
+
+	protected Node(int nbThreads, int nbSchedulableThreads) {
+		super(nbThreads, nbSchedulableThreads);
+		// TODO Auto-generated constructor stub
+	}
+
+}
